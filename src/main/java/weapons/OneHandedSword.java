8f87a0d2b0bc4e5bded8a1ac379c8d73d291866c
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-25 12:16:52 +0100

    weapon name and damage tests

--- src/main/java/weapons/OneHandedSword.java
+++ src/main/java/weapons/OneHandedSword.java
@@ -1,4 +1,12 @@
 package weapons;
 
 public class OneHandedSword {
+    private String name;
+    private double damage;
+
+    public OneHandedSword(String name, double damage){
+        super(name, damage);
+    }
+
+
 }
---
+++ src/test/java/OneHandedSwordTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+import weapons.OneHandedSword;
+
+import static org.junit.Assert.assertEquals;
+
+public class OneHandedSwordTest {
+    OneHandedSword dawnbreaker;
+
+    @Before
+    public void setup(){
+        dawnbreaker = new OneHandedSword("Dawnbreaker", 50);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Dawnbreaker", dawnbreaker.getName());
+    }
+    @Test
+    public void canGetName(){
+        assertEquals("Dawnbreaker", dawnbreaker.getDamage());
+    }
+
+
+}
