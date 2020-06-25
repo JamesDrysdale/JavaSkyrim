98b3c1a8ad46489949c32811d1095a22aeb98ad9
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-25 12:22:17 +0100

    bow has a name and damage

--- src/main/java/weapons/Bow.java
+++ src/main/java/weapons/Bow.java
@@ -1,4 +1,12 @@
 package weapons;
 
-public class Bow {
+public class Bow extends Weapon{
+    private String name;
+    private double damage;
+
+    public Bow(String name, double damage){
+        super(name, damage);
+    }
+
+
 }
--- src/test/java/BowTest.java
+++ src/test/java/BowTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+import weapons.Bow;
+import weapons.OneHandedSword;
+
+import static org.junit.Assert.assertEquals;
+
 public class BowTest {
+    Bow bow;
+
+    @Before
+    public void setup(){
+        bow = new Bow("Glass Bow of the Stag Prince", 30);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Glass Bow of the Stag Prince", bow.getName());
+    }
+    @Test
+    public void canGetDamageValue(){
+        assertEquals(30, bow.getDamage(), 0);
+    }
+
+
 }
