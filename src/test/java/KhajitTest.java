f4f4097881d560286408662fc4e60949fe31b5ff
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-25 11:58:31 +0100

    stat modifiers for khajit stats work

--- src/test/java/KhajitTest.java
+++ src/test/java/KhajitTest.java
@@ -29,5 +29,35 @@ public class KhajitTest {
         assertEquals(130, khajit1.getStamina(), 0);
     }
 
+
+    @Test
+    public void khajitHasMagicPoints(){
+        assertEquals(100, khajit1.getMagic(), 0.0);
+    }
+
+    @Test
+    public void khajitHasBaseMeleeDmg(){
+        assertEquals(1.2, khajit1.getMeleeDmg(), 0.0);
+    }
+
+    @Test
+    public void khajitHasBaseRangeDmg(){
+        assertEquals(1.4, khajit1.getRangeDmg(), 0.0);
+    }
+
+    @Test
+    public void khajitHasBaseMagicDmg(){
+        assertEquals(1.0, khajit1.getMagicDmg(), 0.0);
+    }
+
+    @Test
+    public void khajitHasPhysicalRes(){
+        assertEquals(0.9, khajit1.getPRes(), 0.0);
+    }
+    @Test
+    public void bkhajitHasMagicRes(){
+        assertEquals(1.0, khajit1.getMRes(), 0.0);
+    }
+
 }
 
